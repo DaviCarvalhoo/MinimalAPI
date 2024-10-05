ff3222ab69b1d56828cdbd73d8cb92e1fd4f50a3
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-10-05 14:56:24 -0300

    creating login and adding EntityFrameworkCore

--- Program.cs
+++ Program.cs
@@ -2,5 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (LoginDTO loginDTO) =>{
+  if(loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123")
+    return Results.Ok("Login com sucesso");
+  else
+    return Results.Unauthorized();
+});
+
+
 
 app.Run();
+
+
+public class LoginDTO
+{
+  public string Email { get; set; } = default!;
+  public string Senha { get; set; } = default!;
+}
