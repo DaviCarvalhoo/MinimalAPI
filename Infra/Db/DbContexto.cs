06d457cddf9c38524a72f377c1c2f38368ef506d
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-10-05 15:52:43 -0300

    config database

--- Infra/Db/DbContexto.cs
+++ Infra/Db/DbContexto.cs
@@ -2,11 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Domain.Entitys;
 
 namespace MinimalAPI.Infra.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+        public DbContexto(IConfiguration configuracaoAppSetings)
+        {
+            _configuracaoAppSettings = configuracaoAppSetings;
+        }
+        public DbSet<Administrator> Administrators {get; set;} = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+                if(!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(
+                        stringConexao, 
+                        ServerVersion.AutoDetect(stringConexao)
+                    );
+                }
+            }
+            
+            
+        }
     }
 }
--- obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff3222ab69b1d56828cdbd73d8cb92e1fd4f50a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678d91fff351fa220dcef133c8631b860ab9bae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
